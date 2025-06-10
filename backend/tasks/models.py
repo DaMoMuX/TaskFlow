a2289d6aa311e5839e4ad03d3f06ca47bde8a14e
DaMoMuX <molinamurillodaniel@gmail.com>
2025-06-10 15:18:21 -0500

    Add Category and Task models, register in admin, and apply migrations

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class StatusEnum(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        IN_PROGRESS = 'IN_PROGRESS', 'In Progress'
+        COMPLETED = 'COMPLETED', 'Completed'
+
+class Category(models.Model):
+        name = models.CharField(max_length=100)
+        description = models.TextField(blank=True)
+        
+        def __str__(self):
+                return self.name
+        
+        class Meta:
+                verbose_name_plural = "Categories"
+
+class Task(models.Model):
+        title = models.CharField(max_length=100)
+        description = models.TextField(blank=True)
+        due_date = models.DateField()
+        status = models.CharField(max_length=20, choices=StatusEnum.choices, default=StatusEnum.PENDING)
+        category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+        def __str__(self):
+                return self.title
+
+
